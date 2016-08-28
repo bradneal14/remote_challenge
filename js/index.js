834e6bf081705f296ef19c4e0781d730cf031df6
Bradneal14 <bradleyhneal@gmail.com>
2016-08-28 00:41:46 -0700

    started this repo way too late

---
+++ js/index.js
@@ -0,0 +1,8 @@
+function addCSSClass (elem, className) {
+	removeClassName (elem, className);
+	elem.className = (elem.className + " " + className).trim();
+}
+
+String.prototype.trim = function() {
+	return this.replace( /^\s+|\s+$/, "" );
+}
