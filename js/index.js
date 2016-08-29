ad8f0b5d1b2dfbf6083be54dca47faa0ecd7bbfc
Bradneal14 <bradleyhneal@gmail.com>
2016-08-28 18:03:48 -0700

    fixes table formatting and adds static header bar above table

--- js/index.js
+++ js/index.js
@@ -6,3 +6,24 @@ function addCSSClass (elem, className) {
 String.prototype.trim = function() {
 	return this.replace( /^\s+|\s+$/, "" );
 }
+
+function stripedTable() {
+	if (document.getElementById && document.getElementsByTagName) {
+		var allTables = document.getElementsByTagName('table');
+		if (!allTables) { return; }
+
+		for (var i = 0; i < allTables.length; i++) {
+			if (allTables[i].className.match(/[\w\s ]*scrollTable[\w\s ]*/)) {
+				var trs = allTables[i].getElementsByTagName("tr");
+				for (var j = 0; j < trs.length; j++) {
+					removeClassName(trs[j], 'alternateRow');
+					addCSSClass(trs[j], 'normalRow');
+				}
+				for (var k = 0; k < trs.length; k += 2) {
+					removeClassName(trs[k], 'normalRow');
+					addCSSClass(trs[k], 'alternateRow');
+				}
+			}
+		}
+	}
+}
